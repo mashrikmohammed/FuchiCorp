f9d8df4829dcd69c05e631e823ea88dacaa91b71
mashrik <mashrikmohammed10@gmail.com>
2024-01-31 09:37:32 +0530

    first commit

---
+++ attenndence.js
@@ -0,0 +1,30 @@
+var poPup = document.getElementById("popup-1");
+
+function togglePopup() {
+  poPup.style.display = "block";
+}
+function togglePopupClose() {
+  poPup.style.display = "none";
+}
+
+// Get the  model
+var myModal = document.getElementById('modal');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == myModal) {
+		myModal.style.display = "none";
+		window.history.back();
+	}
+}
+
+// Left bar open nav
+function openNav() {
+	let classnames = document.getElementById('left_menu').className
+	document.getElementById('left_menu').className = classnames.includes('show') ? classnames : `${classnames} show`
+}
+
+// Left bar close nav
+function closeNav() {
+	document.getElementById('left_menu').classList.remove('show')
+}
